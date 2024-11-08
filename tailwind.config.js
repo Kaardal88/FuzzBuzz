24d18e38601e4f8450933e242e2b7b7a5ae2702f
Kaardal88 <123365145+Kaardal88@users.noreply.github.com>
2024-11-08 19:47:08 +0100

    .

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "!./node_modules/**/*"],
+  theme: {
+    extend: {
+      colors: {
+        customOrange: '#FB8B24',
+        customGray: '#333',
+        customTeal: '#0F4C5C',
+        hoverTeal: '#256E82',
+      },
+
+      animation: {
+        arrowBounce: 'arrowBounce 2s linear infinite',
+      },
+      keyframes: {
+        arrowBounce: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(5px)' },
+          '0%': { transform: 'translateY(-5px)' },
+        },
+
+      }
+
+    },
+    plugins: [],
+  }
+}
+
